1396fb10d85b05c489b1caef7c80806f810aea76
rabismail <rabismail@hotech.systems>
2019-12-19 07:59:53 +0300

    add click listener to move to what to recycle screen, and made the whole app theme as NoActionBar

--- app/src/main/java/bisma/rabia/gogreenantalyaadmin/MainActivity.java
+++ app/src/main/java/bisma/rabia/gogreenantalyaadmin/MainActivity.java
@@ -1,14 +1,34 @@
 package bisma.rabia.gogreenantalyaadmin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import bisma.rabia.gogreenantalyaadmin.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final ActivityMainBinding activityMainBinding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_main, null, false);
+        setContentView(activityMainBinding.getRoot());
+
+        activityMainBinding.imvTapToRecycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ThingsToRecycleActivity.class));
+            }
+        });
+        activityMainBinding.txvTapToRecycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activityMainBinding.imvTapToRecycle.callOnClick();
+            }
+        });
     }
 }
